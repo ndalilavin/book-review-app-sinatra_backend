5b810847dbb0a7c2cfe5219bbe603d3d65efab37
ndalilavin <ndalila.steve@gmail.com>
2022-09-13 23:08:21 +0300

    creates table rows

--- db/migrate/20220913200018_create_authors.rb
+++ db/migrate/20220913200018_create_authors.rb
@@ -2,6 +2,8 @@ class CreateAuthors < ActiveRecord::Migration[6.1]
   def change
     create_table :authors do |t|
       t.string :username
+      t.string :book
       t.timestamps
+    end
   end
 end
--- db/migrate/20220913200039_create_reviews.rb
+++ db/migrate/20220913200039_create_reviews.rb
@@ -1,4 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
+    create_table :reviews do |t|
+      t.string :comment
+      t.string :rate
+      t.timestamps
+    end
   end
 end
